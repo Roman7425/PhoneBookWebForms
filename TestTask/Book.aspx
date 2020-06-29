230accae9c4bc10e1d78ec3f88437b71223da0e5
Roman Gubkin <romangubkin1995@gmail.com>
2020-06-29 08:54:38 +0700

    test task ready

---
+++ TestTask/Book.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" EnableEventValidation="false" CodeBehind="Book.aspx.cs" Inherits="TestTask.Book" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" type="text/css" href="Style.css" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="PhoneBook"
+            runat="server"
+            AutoGenerateColumns="false"
+            ShowFooter="true"
+            DataKeyNames="Id"
+            OnRowCommand="PhoneBook_RowCommand"
+            OnRowDeleting="PhoneBook_RowDeleting"
+            BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" OnSelectedIndexChanged="PhoneBook_SelectedIndexChanged"
+            
+            >
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+            <Columns>
+                <asp:TemplateField HeaderText="Name">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Name") %>' runat="server"/>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtName" Text='<%# Eval("Name") %>' runat="server"/>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtNameFooter" runat="server"/>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Surname">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Surname") %>' runat="server"/>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtSurname" Text='<%# Eval("Surname") %>' runat="server"/>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtSurnameFooter" runat="server"/>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Phone">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Phone") %>' runat="server"/>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtPhone" Text='<%# Eval("Phone") %>' runat="server"/>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtPhoneFooter" runat="server"/>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Email">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Email") %>' runat="server"/>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtEmail" Text='<%# Eval("Email") %>' runat="server"/>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtEmailFooter" runat="server"/>
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Button Text="X" CommandName="Delete" runat="server"/>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:Button Text="+" CommandName="AddNew" runat="server"/>
+                    </FooterTemplate>
+                </asp:TemplateField>
+            </Columns>
+
+        </asp:GridView>
+        <br />
+        <asp:Label ID="successMesage" Text="" runat="server" ForeColor="Green"/>
+        <br />
+        <asp:Label ID="errorMessage" Text="" runat="server" ForeColor="Red"/>
+    </form>
+</body>
+</html>
---
+++ TestTask/Book.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestTask
+{
+    public partial class Book : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=91.201.72.213;Initial Catalog=GubkinPhoneBook;User ID=study;Password=study;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+            string persons = "SELECT * FROM PersonTest ORDER BY Id";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlDataAdapter sqlData = new SqlDataAdapter(persons, connection);
+                sqlData.Fill(dt);
+            }
+
+            PhoneBook.DataSource = dt;
+            PhoneBook.DataBind();
+        }
+
+        bool isValid(string email)
+        {
+            string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+            Match isMatch = Regex.Match(email, pattern, RegexOptions.IgnoreCase);
+            return isMatch.Success;
+        }
+
+        protected void PhoneBook_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        protected void PhoneBook_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            successMesage.Text = "";
+            errorMessage.Text = "";
+            if ((PhoneBook.FooterRow.FindControl("txtNameFooter") as TextBox).Text.Trim() == "" ||
+                (PhoneBook.FooterRow.FindControl("txtSurnameFooter") as TextBox).Text.Trim() == "" ||
+                (PhoneBook.FooterRow.FindControl("txtPhoneFooter") as TextBox).Text.Trim() == "" ||
+                (PhoneBook.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim() == "")
+            {
+                errorMessage.Text = "Заполните все поля";
+            }
+            else if (!isValid((PhoneBook.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim()))
+            {
+                errorMessage.Text = "Невалидное значение email!";
+            }
+            else
+            {
+                try
+                {
+                    if (e.CommandName.Equals("AddNew"))
+                    {
+                        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                        {
+                            sqlConnection.Open();
+                            string command = "INSERT INTO PersonTest (Name,Surname,Phone,Email) VALUES (@Name, @Surname, @Phone, @Email)";
+                            SqlCommand add = new SqlCommand(command, sqlConnection);
+                            add.Parameters.AddWithValue("@Name", (PhoneBook.FooterRow.FindControl("txtNameFooter") as TextBox).Text.Trim());
+                            add.Parameters.AddWithValue("@Surname", (PhoneBook.FooterRow.FindControl("txtSurnameFooter") as TextBox).Text.Trim());
+                            add.Parameters.AddWithValue("@Phone", (PhoneBook.FooterRow.FindControl("txtPhoneFooter") as TextBox).Text.Trim());
+                            add.Parameters.AddWithValue("@Email", (PhoneBook.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim());
+                            add.ExecuteNonQuery();
+                            LoadData();
+                            successMesage.Text = "Добавлено!";
+                            errorMessage.Text = "";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    successMesage.Text = "";
+                    errorMessage.Text = "Ошибка!";
+                }
+            }
+        }
+
+        protected void PhoneBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            successMesage.Text = "";
+            errorMessage.Text = "";
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    string command = "DELETE FROM PersonTest WHERE Id = @Id";
+                    SqlCommand add = new SqlCommand(command, sqlConnection);
+                    add.Parameters.AddWithValue("@Id", Convert.ToInt32(PhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    add.ExecuteNonQuery();
+                    LoadData();
+                    successMesage.Text = "Удалено!";
+                    errorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                successMesage.Text = "";
+                errorMessage.Text = "Ошибка!";
+            }
+        }
+    }
+}
